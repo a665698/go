e6b8645ad87d23bd801cc7ccbd777bf96654c339
a665698 <30884897+a665698@users.noreply.github.com>
2018-02-24 16:12:57 +0800

    Create main.go

---
+++ socket/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{}
+
+type client struct {
+	conn *websocket.Conn
+	// message chan []byte
+	name string
+}
+
+var clients map[string]*client
+var addUser chan *client
+var addMessage chan []byte
+
+func main() {
+	fmt.Println("start")
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", fs)
+	http.HandleFunc("/ws", handel)
+	go handelChannel()
+	http.ListenAndServe(":3000", nil)
+}
+
+func handel(w http.ResponseWriter, r *http.Request) {
+	token, err := r.Cookie("token")
+	if err != nil {
+		fmt.Println("token不存在: ", err)
+		return
+	}
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("upgrader错误: ", err)
+		return
+	}
+	var clientItem *client
+	if val, ok := clients[token.Value]; !ok {
+		fmt.Println("用户不存在,创建")
+		// clientItem = &client{conn: c, message: make(chan []byte), name: token.Value}
+		clientItem = &client{conn: c, name: token.Value}
+		addUser <- clientItem
+	} else {
+		fmt.Println("用户存在")
+		clientItem = val
+	}
+
+	// client := &client{conn: c, message: make(chan []byte), name: token.Value}
+	fmt.Println("--61--", len(clients))
+	// go clientItem.writeMes()
+	go clientItem.readMes()
+}
+
+func handelChannel() {
+	clients = make(map[string]*client)
+	addUser = make(chan *client)
+	addMessage = make(chan []byte, 10)
+	for {
+		select {
+		case message := <-addMessage:
+			for _, client := range clients {
+				// select {
+				// case client.message <- message:
+				// default:
+				// 	fmt.Println("--79--close")
+				// 	close(client.message)
+				// 	delete(clients, client.name)
+				// }
+				// client.conn.WriteMessage(1, message)
+
+				client.writeMes(&message)
+			}
+		case user := <-addUser:
+			clients[user.name] = user
+		}
+	}
+}
+
+func (c *client) readMes() {
+	for {
+		mt, message, err := c.conn.ReadMessage()
+		if err != nil {
+			fmt.Println("消息获取失败: ", err, "\n消息类型", mt)
+			if mt == -1 {
+				fmt.Println("--96--close")
+				// close(c.message)
+				delete(clients, c.name)
+			}
+			break
+		}
+		fmt.Println("收到消息: ", string(message), " \n消息类型: ", mt)
+		addMessage <- message
+	}
+}
+
+func (c *client) writeMes(message *[]byte) {
+	// for {
+	// 	select {
+	// 	case message := <-c.message:
+	// 		c.conn.WriteMessage(1, message)
+	// 	}
+	// }
+	err := c.conn.WriteMessage(-1, *message)
+	fmt.Println("---119---", err)
+}
