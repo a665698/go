6d994477a96cc6d2bc20eeb59918adc33730c6e4
lnn <411736397@qq.com>
2018-03-30 18:07:22 +0800

    commit

--- test/main.go
+++ test/main.go
@@ -1,9 +1,31 @@
 package main
 
-
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"io"
+)
 
 func main() {
-
+	file, err := os.OpenFile("te.txt", os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	buf := bufio.NewReader(file)
+	f := ""
+	for {
+		line, err := buf.ReadString('\n')
+		if err != nil || err == io.EOF {
+			fmt.Println(err)
+			break
+		}
+		fmt.Println(line)
+		f += line
+	}
+	os.Truncate("te.txt", 0)
+	file.Write([]byte(f))
 }
 
 
